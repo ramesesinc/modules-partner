d4b67ee5c3c12b1340d4ef3edb2e8989db6a0181
worgflores <worgflores@gmail.com>
2019-11-08 17:17:04 +0800

    updated

--- cloud-server/cloud-partner-services.jar/sql/gdx_partner.sql
+++ cloud-server/cloud-partner-services.jar/sql/gdx_partner.sql
@@ -2,9 +2,7 @@
 select p.*, 
 	c.objid as group_objid, 
 	c.title as group_title,
-	c.name as group_name, 
-	(case when o.id is null then 0 else 1 end) as isonline
+	c.name as group_name
 from partner p 
 	inner join partner_cluster c on c.objid = p.clusterid 
-	left join partner_online o on o.id = p.id 
 order by c.name, length(p.id), p.id 
